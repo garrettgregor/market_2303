b3979f4305fa50e57e71217a6d2654273f936da4
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-17 09:20:59 -0600

    Add item and intialize with name and price as a float

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,1 +1,16 @@
-require 'rspec'
+require "spec_helper"
+
+RSpec.describe Item do
+  describe "#initialize" do
+    it "initializes with attributes" do
+      item1 = Item.new({name: 'Peach', price: "$0.75"})
+      #=> #<Item:0x007f9c56740d48...>
+      item2 = Item.new({name: 'Tomato', price: '$0.50'})
+      #=> #<Item:0x007f9c565c0ce8...>
+      expect(item2.name).to eq("Tomato")
+      #=> "Tomato"
+      expect(item2.price).to eq(0.5)
+      #=> 0.5
+    end
+  end
+end
