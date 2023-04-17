0f0bbacf604b8b9c049c366e362f5f970cf28ba2
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-17 10:16:04 -0600

    Add vendor names method to create list of vendor names from vendor list

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -11,7 +11,33 @@ RSpec.describe Market do
   end
   
   describe "#add_vendor" do
-    it "initializes with name and empty collection of vendors" do
+    it "add vendors to market" do
+      market = Market.new("South Pearl Street Farmers Market")
+      item1 = Item.new({name: 'Peach', price: "$0.75"})
+      item2 = Item.new({name: 'Tomato', price: "$0.50"})
+      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+      vendor1 = Vendor.new("Rocky Mountain Fresh")
+      vendor1.stock(item1, 35)    
+      vendor1.stock(item2, 7)    
+      
+      vendor2 = Vendor.new("Ba-Nom-a-Nom")    
+      vendor2.stock(item4, 50)    
+      vendor2.stock(item3, 25)
+      
+      vendor3 = Vendor.new("Palisade Peach Shack")    
+      vendor3.stock(item1, 65)  
+
+      market.add_vendor(vendor1)    
+      market.add_vendor(vendor2)    
+      market.add_vendor(vendor3)
+      
+      expect(market.vendors).to eq([vendor1, vendor2, vendor3])
+    end
+  end
+  
+  describe "#vendor_names" do
+    it "creates a list of vendor names from list of vendors" do
       market = Market.new("South Pearl Street Farmers Market")
       item1 = Item.new({name: 'Peach', price: "$0.75"})
       #=> #<Item:0x007f9c56740d48...>
@@ -38,9 +64,11 @@ RSpec.describe Market do
       market.add_vendor(vendor1)    
       market.add_vendor(vendor2)    
       market.add_vendor(vendor3)
-     
+      
       expect(market.vendors).to eq([vendor1, vendor2, vendor3])
       #=> [#<Vendor:0x00007fe1348a1160...>, #<Vendor:0x00007fe1349bed40...>, #<Vendor:0x00007fe134910650...>]
+      expect(market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+      #=> ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
     end
   end
 end
