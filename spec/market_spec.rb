0b71474595df30135c1d349107e4cae2c7c4229d
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-17 10:04:23 -0600

    Add empty collection of vendors to initialize market

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -2,10 +2,11 @@ require "spec_helper"
 
 RSpec.describe Market do
   describe "#initialize" do
-    it "initializes with name" do
+    it "initializes with name and empty collection of vendors" do
       market = Market.new("South Pearl Street Farmers Market")
 
       expect(market.name).to eq("South Pearl Street Farmers Market")
+      expect(market.vendors).to eq([])
     end
   end
 end
