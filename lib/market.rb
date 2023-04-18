8d1b1ec61f2ef0cb15d866500c3d876d67d935fb
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-18 11:15:29 -0600

    Revise: class review

--- lib/market.rb
+++ lib/market.rb
@@ -39,15 +39,65 @@ class Market
   end
 
   def total_inventory
-    total_inventory = Hash.new(0)
+    # total_inventory = Hash.new(0)
+    # @vendors.each do |vendor|
+    #   vendor.inventory.each do |item, stock|
+    #     total_inventory[item] = Hash.new(0)
+    #     {
+    #       quantity: total_inventory
+    #       vendors: vendors_that_sell(item)
+    #     }
+    #   end
+    # end
+    # total_inventory
+    total = {}
     @vendors.each do |vendor|
-      vendor.inventory.each do |item, stock|
-        total_inventory[item] = {
-          :quantity => stock,
-          :vendors  => vendors_that_sell(item)
-        }
+      vendor.inventory.each do |item, quantity|
+        if total[item]
+          total[item][:quantity] += quantity
+        else
+          total[item] = {
+            quantity: quantity,
+            vendors: vendors_that_sell(item)
+          }
+        end
       end
     end
-    total_inventory
+    total
+  end
+
+  def overstocked_items
+    # overstocked = []
+    # total_inventory.select do |item, info|
+    #   if info[:quantity] > 50 && vendors_that_sell(item).length > 1
+    #     overstocked << item
+    #   end
+    # end
+    # overstocked
+    overstocked = total_inventory.select do |item, info|
+      info[:quantity] > 50 && vendors_that_sell(item).length > 1
+    end
+    overstocked.keys
+  end
+
+  def date
+
+  end
+
+  def sell(item, quantity)
+    # guard clause
+    return false if total_inventory[item][:quantity] < quantity
+    @vendors.each do |vendor|
+      if vendor.check_stock(item) > quantity
+        vendor.inventory[item] -= quantity
+        # the break gets out of the enumeration or iteration currently in if the above is true
+        # the desired quantity has been satisfied, which takes you out of the .each, without it
+        break
+      else
+        quantity -= vendor.inventory[item]
+        vendor.inventory[item] = 0
+      end
+    end
+    return true
   end
 end
