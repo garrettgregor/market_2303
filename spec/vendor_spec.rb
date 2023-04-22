52aaf0e790eeaa53fdd3de33cc82805689499eb9
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-21 20:39:45 -0600

    Additional tests for check stock

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -37,5 +37,23 @@ RSpec.describe Vendor do
       expect(vendor.check_stock(item1)).to eq(30)
       expect(vendor.inventory).to eq(expected)
     end
+
+    it "can add stock of multiple items" do
+      vendor = Vendor.new("Rocky Mountain Fresh")
+      item1 = Item.new({name: 'Peach', price: "$0.75"})
+      item2 = Item.new({name: 'Tomato', price: '$0.50'})
+      vendor.stock(item1, 30)
+      vendor.stock(item1, 25)
+      vendor.stock(item2, 12)
+
+      expected = {
+        item1 => 55,
+        item2 => 12
+      }
+
+      expect(vendor.check_stock(item1)).to eq(55)
+      expect(vendor.check_stock(item2)).to eq(12)
+      expect(vendor.inventory).to eq(expected)
+    end
   end
 end
