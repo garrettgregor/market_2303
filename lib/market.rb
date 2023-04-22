973579acd760d2c5edb4935e52d4e91f1c4fb8d7
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-21 22:04:38 -0600

    Add total inventory to market

--- lib/market.rb
+++ lib/market.rb
@@ -23,4 +23,24 @@ class Market
     end
   end
 
+  def total_inventory
+    total_inventory = {}
+
+    @vendors.each do |vendor, name|
+      vendor.inventory.each do |item, quantity|
+        if !total_inventory[item]
+          total_inventory[item] = {
+            quantity: 0,
+            vendors: vendors_that_sell(item)
+          }
+          total_inventory[item][:quantity] += quantity
+        elsif total_inventory[item]
+          total_inventory[item][:quantity] += quantity
+        end
+      end
+    end
+    
+    total_inventory
+  end
+
 end
