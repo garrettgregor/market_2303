2b397d0da8600fdf2a3f4124208057bdd9ed1826
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-21 20:32:36 -0600

    Add check stock method to vendor

---
+++ spec/vendor_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+RSpec.describe Vendor do
+  describe "#initialize" do
+    it "exists and has attributes" do
+      vendor = Vendor.new("Rocky Mountain Fresh")
+
+      expect(vendor).to be_a Vendor
+      expect(vendor.name).to eq("Rocky Mountain Fresh")
+      expect(vendor.inventory).to eq({})
+    end
+  end
+
+  describe "#check_stock" do
+    it "can check the stock of an item" do
+      vendor = Vendor.new("Rocky Mountain Fresh")
+      item1 = Item.new({name: 'Peach', price: "$0.75"})
+      item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+      expect(vendor).to be_a Vendor
+      expect(vendor.name).to eq("Rocky Mountain Fresh")
+      expect(vendor.check_stock(item1)).to eq(0)
+    end
+  end
+end
